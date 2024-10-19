c3ab374e07579e087e657428cbba7c1ef42f13a5
ki11errabbit <ki11errabbit@posteo.net>
2024-10-18 19:55:14 -0600

    added a bunch of symbolic icons

--- src/actions/convert.sh
+++ src/actions/convert.sh
@@ -23,3 +23,18 @@ for file in *.svg; do
     sed -i "s/rgb(100%, 100%, 100%)/$color/g" "$output/$file"
     echo "Converted $file"
 done
+mkdir -p "$output/symbolic"
+for file in symbolic/*.svg; do
+    case "$file_type" in
+        png)
+            rsvg-convert -a -w "$size" -h "$size" -f png -o "$output/${file%.*}.png" "$output/$file"
+            rm "$output/$file"
+            echo "Converted $file"
+            continue
+            ;;
+        *)
+            ;;
+    esac
+    rsvg-convert -a -w "$size" -f svg -o "$output/$file" $file
+    echo "Converted $file"
+done
