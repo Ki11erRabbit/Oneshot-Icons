4e30ec061213fefeb8c8d74f8a62fa58ff7abf15
ki11errabbit <ki11errabbit@posteo.net>
2024-10-27 12:05:08 -0600

    added convert script to mimetypes

---
+++ src/mimetypes/convert.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+size=$1
+color=$2
+file_type=$3
+output=$4
+
+
+for file in *.svg; do
+    case "$file_type" in
+        png)
+            rsvg-convert -a -w "$size" -f svg -o "$output/$file" $file
+            sed -i "s/rgb(100%, 100%, 100%)/$color/g" "$output/$file"
+            rsvg-convert -a -w "$size" -h "$size" -f png -o "$output/${file%.*}.png" "$output/$file"
+            rm "$output/$file"
+            echo "Converted $file"
+            continue
+            ;;
+        *)
+            ;;
+    esac
+    rsvg-convert -a -w "$size" -f svg -o "$output/$file" $file
+    sed -i "s/rgb(100%, 100%, 100%)/$color/g" "$output/$file"
+    echo "Converted $file"
+done
+mkdir -p "$output/symbolic"
+for file in ./symbolic/*.svg; do
+    case "$file_type" in
+        png)
+            rsvg-convert -a -w "$size" -h "$size" -f png -o "$output/${file%.*}.png" "$output/$file"
+            echo "Converted $file"
+            continue
+            ;;
+        *)
+            ;;
+    esac
+    rsvg-convert -a -w "$size" -f svg -o "$output/$file" $file
+    echo "Converted $file"
+done
